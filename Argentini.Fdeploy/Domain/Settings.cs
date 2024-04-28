27b778fad8d2ab8e6f771d4952a3203f672eea7c
Michael Argentini <michael@argentini.us>
2024-04-28 00:24:00 -0400

    Removed OverwriteWebConfig property

--- Argentini.Fdeploy/Domain/Settings.cs
+++ Argentini.Fdeploy/Domain/Settings.cs
@@ -3,7 +3,6 @@ namespace Argentini.Fdeploy.Domain;
 public sealed class Settings
 {
     public bool DeleteOrphans { get; set; } = true;
-    public bool OverwriteWebConfig { get; set; } = true;
     public bool TakeServerOffline { get; set; } = true;
     
     public int ServerOfflineDelaySeconds { get; set; } = 10;
